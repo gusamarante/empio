adb968bbe9b23f36e0b18560d849c2d9caa5f283
gustavoamarante <gusamarante@gmail.com>
2025-05-11 18:09:14 -0300

    starting elasticities

--- problem-set-1/problem-1/item-a.py
+++ problem-set-1/problem-1/item-a.py
@@ -10,7 +10,10 @@ rest_dummies = pd.get_dummies(data['restaurant'], prefix="asc").astype(int)
 data = pd.concat([data, rest_dummies], axis=1)
 data = data.drop(f"asc_{outside_option}", axis=1)
 
-# Estimate mlogit
+
+# ===========================
+# ===== Estimate mlogit =====
+# ===========================
 vars_x = [
     'asc_CafeEccell',
     'asc_Christophers',
@@ -32,3 +35,35 @@ model.fit(
     alts=data["restaurant"]
 )
 print(model.summary())
+coeffs = pd.Series(index=vars_x, data=model.coeff_)
+
+
+# ================================
+# ===== Compute elasticities =====
+# ================================
+# Fitted probabilities
+_, probs = model.predict(
+    X=data[vars_x],
+    varnames=vars_x,
+    ids=data["family.id"],
+    alts=data["restaurant"],
+    return_proba=True,
+)
+probs = pd.DataFrame(data=probs, columns=model.alternatives)
+
+# marginal effects and elasticities
+me = pd.DataFrame(columns=model.alternatives)
+el = pd.DataFrame(columns=model.alternatives)
+wrt = "Freebirds"
+for rest in model.alternatives:
+
+    if rest == wrt:
+        me[rest] = 1
+    else:
+        me[rest] = coeffs.loc['cost']
+
+
+# TODO report qunatiles of elasticities
+# TODO chart of elasticity per alternative
+
+a = 1
