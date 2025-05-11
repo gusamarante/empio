6ab6633a494328c17bcdaae903973c7f41b264ca
gustavoamarante <gusamarante@gmail.com>
2025-05-11 10:50:14 -0300

    model estimate

--- problem-set-1/problem-1/item-a.py
+++ problem-set-1/problem-1/item-a.py
@@ -1,5 +1,34 @@
 from empio.data import load_restaurant
+from xlogit import MultinomialLogit
+import pandas as pd
 
 data = load_restaurant(data_format="long")
 
-print(data)
+# add alternative specific constants (asc)
+outside_option = "Freebirds"
+rest_dummies = pd.get_dummies(data['restaurant'], prefix="asc").astype(int)
+data = pd.concat([data, rest_dummies], axis=1)
+data = data.drop(f"asc_{outside_option}", axis=1)
+
+# Estimate mlogit
+vars_x = [
+    'asc_CafeEccell',
+    'asc_Christophers',
+    # 'asc_Freebirds',
+    'asc_LosNortenos',
+    'asc_MadCows',
+    'asc_MamasPizza',
+    'asc_WingsNmore',
+    'cost',
+    'distance',
+]
+
+model = MultinomialLogit()
+model.fit(
+    X=data[vars_x],
+    y=data["choice"],
+    varnames=vars_x,
+    ids=data["family.id"],
+    alts=data["restaurant"]
+)
+print(model.summary())
