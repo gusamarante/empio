ea2e3b5550f66a4d0898c63453565079745b1162
gamarante <gustavo.amarante@bwgi.com.br>
2025-06-23 10:48:03 -0300

    trying files from CVM

--- research/cvm.py
+++ research/cvm.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("cda_fi_202505/cda_fi_BLC_2_202505.csv", sep=';', encoding='cp860')
+df = pd.read_csv("cda_fi_BLC_4_202505.csv", sep=';', encoding='cp860')
 
 print(df)
+df.to_clipboard()
